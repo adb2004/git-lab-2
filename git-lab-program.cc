a5b8cb27f2999a11dbf36a7202db8e061dda7a70
Ashley Brock <ab904621@ohio.edu>
2023-09-08 17:47:43 -0400

    hi

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Ashley Brock>
+ *        Date: <9/8/23>
+ * Description: Gitlab code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+    return 0;
+}// main
